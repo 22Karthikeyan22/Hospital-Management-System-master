2b5d5388c99aa5149c5d10849bd6ae0d1a3f81c9
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-12-29 00:26:17 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const db = mysql.createConnection({
     host    : 'localhost',
-    user    : 'root',
-    password: '',
+    user    : 'karthi_2002',
+    password: 'mano@2002',
     database: 'HospitalManagementSystem',
     port: 3306
 });
